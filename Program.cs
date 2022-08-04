496ae12cef1a820aae7ff8f0ca44175c44fb3656
Timofei Shadrin <tshadrin@yandex.ru>
2022-08-04 12:49:27 +0300

    Base code created

--- Program.cs
+++ Program.cs
@@ -4,8 +4,27 @@
     {
         public static void Main(string[] args)
         {
-            int count = Methods.ConditionForFill();
-            Console.WriteLine("\tCount: " + count);
+            string text = "Do you can cook? My friend, Bob!";
+            text = text.ToLower();
+            char[] symbols = { ';', '.', ',', '!', '?', ':' };
+            char[] stringArray = text.ToCharArray();
+            foreach (char c in symbols)
+            {
+                for (int i = 0; i < stringArray.Length; i++)
+                    if (stringArray[i] == c)
+                        stringArray[i] = ' ';
+            }
+
+            text = String.Join("", stringArray);
+            string[] result = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("[" + String.Join(", ", result) + "]");
+            List<string> resultList = new List<string>();
+            foreach (string word in result)
+            {
+                if (word.Length <= 3)
+                    resultList.Add(word);
+            }
+            Console.WriteLine("[" + String.Join(", ", resultList) + "]");
         }
     }
 }
