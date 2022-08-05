a28ce1d76fab7eb6ccff7a5a1dafd3f30f67d294
Timofei Shadrin <tshadrin@yandex.ru>
2022-08-05 18:03:03 +0300

    Fill array test

--- Methods.cs
+++ Methods.cs
@@ -2,17 +2,40 @@ namespace ControlWork
 {
     class Methods
     {
-        public static byte ConditionForFill()
+        public static string[] array = new string[0];
+
+        public static void FillStringArray()
         {
-            byte count = 0;
+            Console.Clear();
             Console.Write(
-                "\n\tПожалуйста введите введите количество вводимых строк\n"
-                    + "\tв виде натурального числа: "
+                "\n\tПожалуйста введите несколько строк для заполнения массива.\n"
+                    + "\tЧтобы остановить процесс нажмите Escape.\n"
             );
-            string? number = Console.ReadLine();
-            if (!string.IsNullOrEmpty(number))
-                count = Convert.ToByte(number.ToString());
-            return count;
+            int i = 0;
+            Array.Resize(ref array, 0);
+            ConsoleKeyInfo choise;
+            while (true)
+            {
+                Console.Write($"\tВведите элемнт c индексом No {i}: ");
+                string? str = Console.ReadLine();
+                if (!String.IsNullOrEmpty(str))
+                    AddToArray(ref array, str);
+                Console.Write($"\tПродолжить ввод? ");
+                choise = Console.ReadKey();
+                Console.WriteLine();
+                if (choise.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                i++;
+            }
+        }
+
+        public static void AddToArray(ref string[] arrayOfStrings, string text)
+        {
+            int lenght = arrayOfStrings.Length;
+            Array.Resize(ref arrayOfStrings, lenght + 1);
+            array[arrayOfStrings.Length - 1] = text;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,27 +4,33 @@
     {
         public static void Main(string[] args)
         {
-            string text = "Do you can cook? My friend, Bob!";
-            text = text.ToLower();
-            char[] symbols = { ';', '.', ',', '!', '?', ':' };
-            char[] stringArray = text.ToCharArray();
-            foreach (char c in symbols)
-            {
-                for (int i = 0; i < stringArray.Length; i++)
-                    if (stringArray[i] == c)
-                        stringArray[i] = ' ';
-            }
+            // string text = "Do you can cook? My friend, Bob!";
+            // text = text.ToLower();
+            // string stringOfSymbols = ":;'/?.>,<[]{}|`~!@#$%^&*()+-/";
+            // char[] symbols = stringOfSymbols.ToCharArray();
+            // char[] stringArray = text.ToCharArray();
+            // foreach (char c in symbols)
+            // {
+            //     for (int i = 0; i < stringArray.Length; i++)
+            //         if (stringArray[i] == c)
+            //             stringArray[i] = ' ';
+            // }
 
-            text = String.Join("", stringArray);
-            string[] result = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            Console.WriteLine("[" + String.Join(", ", result) + "]");
-            List<string> resultList = new List<string>();
-            foreach (string word in result)
-            {
-                if (word.Length <= 3)
-                    resultList.Add(word);
-            }
-            Console.WriteLine("[" + String.Join(", ", resultList) + "]");
+            // text = String.Join("", stringArray);
+            // string[] result = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            // Console.WriteLine("[" + String.Join(", ", result) + "]");
+            // List<string> resultList = new List<string>();
+            // foreach (string word in result)
+            // {
+            //     if (word.Length <= 3)
+            //         resultList.Add(word);
+            // }
+            // Console.WriteLine("[" + String.Join(", ", resultList) + "]");
+            Methods.FillStringArray();
+            Console.Clear();
+            Console.WriteLine("В");
+            Console.WriteLine("Вы ввели следующие строки:");
+            Console.WriteLine("[" + String.Join(", ", Methods.array) + "]");
         }
     }
 }
